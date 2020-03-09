7e70235afa92999882af1388c4bd42e056b4c3a5
Elshad Seyidmammadov <elshad@eclipsesource.com>
2020-03-09 10:30:51 +0000

    Update app file

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import './ui/SubredditPage';
 import './ui/SubredditSelectorView';
 import './ui/ViewModeToggleAction';
 
-// sssss aaaa
+console.log('This is as test change!');
 
 resolve(SubredditSelectorPresenter).subreddits = DEFAULT_REDDITS;
 resolve(Navigation).navigateTo(resolve(SubredditPresenter).view);
